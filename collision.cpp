1255045f2f2c47a2739f1a24d856fb93d49b599d
Rafay Ghafoor <rafayghafoor@pm.me>
2018-12-05 10:25:12 +0500

    Update collision.cpp

--- collision.cpp
+++ collision.cpp
@@ -1,16 +1,14 @@
-bool IsColliding(int left, int top, int right, int bottom, int left1, int top1, int right1, int bottom1)
+bool IsColliding(int left, int top, int right, int bottom, int left1, int top1, int right1, int bottom1, int radius=50)
 
 {
-	int distance = 50;
 	int x = (top - left) / 2 + left;
 	int y = (bottom - right) / 2 + right;
 	int x1 = (top1 - left1) / 2 + left1;
 	int y1 = (bottom1 - right1) / 2 + right1;
 
 
-	if (sqrt(((x1 - x)*(x1 - x)) + ((y1 - y)*(y1 - y))) <= 50)
+	if (sqrt(((x1 - x)*(x1 - x)) + ((y1 - y)*(y1 - y))) <= radius)
 		return true;
-
-	else
-		return false;
+	
+	return false;
 }
